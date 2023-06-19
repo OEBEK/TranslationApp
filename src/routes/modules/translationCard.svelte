b7b6765a6cbf7d1e2d7cac74119cb07b0dc2a361
Reykone <oemer.bektuerk@gmx.de>
2023-06-19 12:08:52 +0200

    added card

---
+++ src/routes/modules/translationCard.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let headword = "";
+</script>
+
+<div class="mt-4">
+    <h3 class="text-lg font-bold">{headword}</h3>
+    <table class="w-full border border-gray-300">
+        <thead>
+            <tr>
+                <th class="p-2 border-b border-gray-300">German</th>
+                <th class="p-2 border-b border-gray-300">English</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="p-2 border-b border-gray-300">Haus</td>
+                <td class="p-2 border-b border-gray-300">House</td>
+            </tr>
+            <!-- Weitere Übersetzungen hinzufügen -->
+        </tbody>
+    </table>
+</div>
--- src/routes/scripts/requets.js
+++ src/routes/scripts/requets.js
@@ -1,14 +1,28 @@
-let backendUrl = "http://0.0.0.0:8000"
+// Define backendUrl
+const backendUrl = "http://0.0.0.0:8000";
 
-
+// Define getTranslation function
 export function getTranslation(word, targetLanguage, sourceLanguage) {
-    let response = fetch(backendUrl + "/translate/" + word + "&targetLanguage=" + targetLanguage + "sourceLanguage=" + sourceLanguage, {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        }
-      })
+  let response = fetch(
+    backendUrl + "/translate/" + word + "&targetLanguage=" + targetLanguage + "sourceLanguage=" + sourceLanguage,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-    response.then(test => console.log(test.json()))
+  response
+    .then((test) => test.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
 }
+
+// Call getTranslation with appropriate arguments
+getTranslation("hello", "es", "en");
+
+// ./gradlew.bat shadowJar
+
+// ./build/libs java -jar 
