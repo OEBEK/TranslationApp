198add52c25f0f8baa2250f55cc2576d3d935926
Juli0q <juold@proton.me>
2023-06-20 23:19:55 +0200

    Fix url

--- src/routes/scripts/requets.js
+++ src/routes/scripts/requets.js
@@ -1,7 +1,7 @@
 // Define backendUrl
 const backendUrl = "http://0.0.0.0:8000";
 export function getTranslation(word, targetLanguage, sourceLanguage) {
-  return fetch(backendUrl + "/translate/" + word + "&targetLanguage=" + targetLanguage + "sourceLanguage=" + sourceLanguage, {
+  return fetch(backendUrl + "/translate/" + word + "&targetLanguage=" + targetLanguage + "&sourceLanguage=" + sourceLanguage, {
       method: "GET",
       mode: "cors",
       headers: {
